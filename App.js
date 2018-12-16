20c2f3c2717fcc9d2501c5fb1c4b1c945bda3c65
b33 <b33red@gmail.com>
2018-12-16 15:39:12 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import ScrollSwagger from "./src/ScrollSwagger";
+
+export default class App extends React.Component {
+  render() {
+    return <ScrollSwagger />;
+  }
+}
